42c212c5de17dde12642652ac7ea33f66d81eda6
apobletts <anna.pobletts@gmail.com>
2022-01-28 09:26:00 -0600

    add index to passage_id

--- 02-Custom-Registration/app/models.py
+++ 02-Custom-Registration/app/models.py
@@ -4,4 +4,4 @@ from app import db
 class User(db.Model):
    id = db.Column(db.Integer, primary_key = True,autoincrement=True)
    name = db.Column(db.String(100))
-   passage_id = db.Column(db.String(24)) 
+   passage_id = db.Column(db.String(24), index=True) 
